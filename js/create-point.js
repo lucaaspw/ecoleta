899fc26c1106b3c6885b270211c8d02c522f2b56
lucas gonçalves <lucas.goncalves@foster.com.br>
2020-06-14 11:17:13 -0300

    Front-end da aplicação

---
+++ js/create-point.js
@@ -0,0 +1,38 @@
+
+function ufState(){
+    const ufSelect = document.querySelector('[name=uf]');
+    fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+    .then(res => res.json())
+    .then( states =>{
+        for(const state of states){
+            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+        }
+    })
+}
+ufState();
+
+document.querySelector('[name=uf]').addEventListener('change', getCities)
+
+var optionValue = document.querySelector('.firstOption')
+function getCities(event){
+    const citySelect = document.querySelector('[name=city]');
+    const inputState = document.querySelector('[name=state]');
+    const ufValue = event.target.value;
+    const indexOfState = event.target.selectedIndex
+    inputState.value = event.target.options[indexOfState].text
+
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios
+    `
+    citySelect.innerHTML = "<option value>Selecione a cidade</option>"
+    if(optionValue.value == 'primeiro'){
+        citySelect.disabled = true;
+    }
+    fetch(url).then(res => res.json())
+    .then( cities => {
+        
+        for(const city of cities){
+            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+        }
+            citySelect.disabled = false;
+    })
+}
